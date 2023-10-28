1ccbb767c9dce38f7e04c88a1edab92f9002ae4a
ramakrishna-raj <ramakrishna.rk0902@gmail.com>
2023-10-28 16:23:36 +0000

    update with pub ip

--- vpc.tf
+++ vpc.tf
@@ -15,6 +15,7 @@ resource "aws_subnet" "application-pub-sn-A" {
   vpc_id     = aws_vpc.application.id
   cidr_block = "192.168.1.0/24"
   availability_zone = "ap-southeast-2a"
+  map_public_ip_on_launch = "true"
   tags = {
     Name = "rama-pub-sn-A"
   }
