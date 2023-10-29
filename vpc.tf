0d4d9566496701039ef5c63ce537330e9e3d1e6d
ramakrishna-raj <ramakrishna.rk0902@gmail.com>
2023-10-29 15:29:26 +0000

    create pvt sn AZ-A&B

--- vpc.tf
+++ vpc.tf
@@ -32,3 +32,27 @@ resource "aws_subnet" "application-pub-sn-B" {
     Name = "rama-pub-sn-B"
   }
 }
+
+# Create Private subnet AZ-A
+
+resource "aws_subnet" "application-pvt-sn-A" {
+  vpc_id     = aws_vpc.application.id
+  cidr_block = "192.168.3.0/24"
+  availability_zone = "ap-southeast-2a"
+  map_public_ip_on_launch = "false"
+  tags = {
+    Name = "rama-pvt-sn-A"
+  }
+}
+
+# Create Private subnet AZ-B
+
+resource "aws_subnet" "application-pvt-sn-B" {
+  vpc_id     = aws_vpc.application.id
+  cidr_block = "192.168.4.0/24"
+  availability_zone = "ap-southeast-2b"
+  map_public_ip_on_launch = "false"
+  tags = {
+    Name = "rama-pvt-sn-B"
+  }
+}
