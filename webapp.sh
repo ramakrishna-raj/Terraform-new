50ba33d8412054d4e8d141c5d0051298fe97aa04
ramakrishna-raj <ramakrishna.rk0902@gmail.com>
2023-10-30 15:28:52 +0000

    Script for Ecomm app

---
+++ webapp.sh
@@ -0,0 +1,7 @@
+# Script for Ecomm Application
+
+#!/bin/bash
+sudo apt update -y
+sudo apt install nginx -y
+sudo rm -rf /var/www/html/*
+sudo git clone https://github.com/ravi2krishna/ecomm.git /var/www/html
